6d26d1dd67116179855379f825da6cdae15579a8
pete <pete@qubitsec.com>
2024-07-12 16:09:35 +0900

    init

---
+++ src/main/java/com/example/claudedemo/ClaudeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.claudedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaudeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaudeDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/claudedemo/ClaudeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.claudedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaudeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
